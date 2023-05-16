8b47b075b124553de475317c53a581b1ff136c45
Zachary Garwood <garwoodzachary@gmail.com>
2023-05-16 13:46:49 -0500

    Moved the board into board.rs

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,9 @@
-mod pieces;
+mod board;
 mod defs;
-use pieces::{Pieces, Color};
-use defs::*;
+mod pieces;
+use board::Board;
 
 fn main() {
     let board = Board::new();
     board.print_board();
-}
-
-struct Board {
-    board: [Pieces; 128],
-    turn_color: Color,
-}
-impl Board {
-    fn new() -> Self {
-        use pieces::{Pieces::*, Color::*};
-        Self {
-            board: [ // 0x88 Board Representation
-                r, n, b, q, k, b, n, r,  o, o, o, o, o, o, o, o, 
-                p, p, p, p, p, p, p, p,  o, o, o, o, o, o, o, o, 
-                e, e, e, e, e, e, e, e,  o, o, o, o, o, o, o, o, 
-                e, e, e, e, e, e, e, e,  o, o, o, o, o, o, o, o,
-                e, e, e, e, e, e, e, e,  o, o, o, o, o, o, o, o,
-                e, e, e, e, e, e, e, e,  o, o, o, o, o, o, o, o,
-                P, P, P, P, P, P, P, P,  o, o, o, o, o, o, o, o,
-                R, N, B, Q, K, B, N, R,  o, o, o, o, o, o, o, o, 
-            ], 
-            turn_color: WHITE
-        }
-    }
-    fn print_board(&self) {
-        println!();
-        for rank in 0..RANKS {
-            for file in 0..FILES {
-                let square = get_square(rank, file);
-
-                // Print ranks
-                if file == 0 {
-                    print!(" {}  ", 8 - rank);
-                }
-
-                // Print board contents, avoiding the off board values
-                if (square & 0x88) == 0 {
-                    print!(" {} ", self.board[square])
-                }
-            }
-            println!();
-        }
-        println!("\n     a  b  c  d  e  f  g  h")
-    }
-}
-
-fn get_file(square: usize) -> usize {
-    square & 7
-}
-
-fn get_rank(square: usize) -> usize {
-    square >> 4
-}
-
-fn get_square(rank: usize, file: usize) -> usize {
-    (rank << 4) + file
 }
