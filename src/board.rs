24dc9966facfc50b0df072d1a21506827df620b7
Zachary Garwood <garwoodzachary@gmail.com>
2023-05-16 15:38:32 -0500

    Added board representation from a FEN

--- src/board.rs
+++ src/board.rs
@@ -1,5 +1,5 @@
-use crate::pieces::{Pieces, Color};
 use crate::defs::*;
+use crate::pieces::{Color, Pieces};
 
 pub struct Board {
     board: [Pieces; 128],
@@ -7,31 +7,79 @@ pub struct Board {
 }
 impl Board {
     pub fn new() -> Self {
-        use crate::pieces::{Pieces::*, Color::*};
+        use crate::pieces::{Color::*, Pieces::*};
         Self {
-            board: [ // 0x88 Board Representation
-                r, n, b, q, k, b, n, r,  o, o, o, o, o, o, o, o, 
-                p, p, p, p, p, p, p, p,  o, o, o, o, o, o, o, o, 
-                e, e, e, e, e, e, e, e,  o, o, o, o, o, o, o, o, 
-                e, e, e, e, e, e, e, e,  o, o, o, o, o, o, o, o,
-                e, e, e, e, e, e, e, e,  o, o, o, o, o, o, o, o,
-                e, e, e, e, e, e, e, e,  o, o, o, o, o, o, o, o,
-                P, P, P, P, P, P, P, P,  o, o, o, o, o, o, o, o,
-                R, N, B, Q, K, B, N, R,  o, o, o, o, o, o, o, o, 
-            ], 
-            turn_color: WHITE
+            board: [
+                // 0x88 Board Representation
+                r, n, b, q, k, b, n, r, o, o, o, o, o, o, o, o, p, p, p, p, p, p, p, p, o, o, o, o,
+                o, o, o, o, e, e, e, e, e, e, e, e, o, o, o, o, o, o, o, o, e, e, e, e, e, e, e, e,
+                o, o, o, o, o, o, o, o, e, e, e, e, e, e, e, e, o, o, o, o, o, o, o, o, e, e, e, e,
+                e, e, e, e, o, o, o, o, o, o, o, o, P, P, P, P, P, P, P, P, o, o, o, o, o, o, o, o,
+                R, N, B, Q, K, B, N, R, o, o, o, o, o, o, o, o,
+            ],
+            turn_color: WHITE,
+        }
+    }
+
+    pub fn set_board_from_fen(&mut self, fen: &str) {
+        // Clear the board
+        self.reset_board();
+
+        // Split the FEN string into separate components
+        let fen_parts: Vec<&str> = fen.split(' ').collect();
+        let fen_board = fen_parts[0];
+
+        // Iterate over the FEN board representation and populate the actual board
+        let mut file = 0;
+        let mut rank = RANKS - 1;
+
+        // Used for modifying the file number
+        let mut special_fen_char = false;
+
+        for fen_char in fen_board.chars() {
+            match fen_char {
+                'r' => self.set_square(rank, file, Pieces::r),
+                'n' => self.set_square(rank, file, Pieces::n),
+                'b' => self.set_square(rank, file, Pieces::b),
+                'q' => self.set_square(rank, file, Pieces::q),
+                'k' => self.set_square(rank, file, Pieces::k),
+                'p' => self.set_square(rank, file, Pieces::p),
+                'R' => self.set_square(rank, file, Pieces::R),
+                'N' => self.set_square(rank, file, Pieces::N),
+                'B' => self.set_square(rank, file, Pieces::B),
+                'Q' => self.set_square(rank, file, Pieces::Q),
+                'K' => self.set_square(rank, file, Pieces::K),
+                'P' => self.set_square(rank, file, Pieces::P),
+                '/' => {
+                    rank -= 1;
+                    file = 0;
+                    special_fen_char = true;
+                }
+                _ => {
+                    if let Some(spaces) = fen_char.to_digit(10) {
+                        file += spaces as usize;
+                        special_fen_char = true;
+                    }
+                }
+            }
+
+            // Special chars already changed file
+            if !special_fen_char {
+                file += 1;
+            }
+            special_fen_char = false;
         }
     }
 
     pub fn print_board(&self) {
         println!();
-        for rank in 0..RANKS {
+        for rank in (0..RANKS).rev() {
             for file in 0..FILES {
                 let square = Self::get_square(rank, file);
 
                 // Print ranks
                 if file == 0 {
-                    print!(" {}  ", 8 - rank);
+                    print!(" {}  ", rank);
                 }
 
                 // Print board contents, avoiding the off board values
@@ -41,18 +89,30 @@ impl Board {
             }
             println!();
         }
-        println!("\n     a  b  c  d  e  f  g  h")
+        println!("\n     a  b  c  d  e  f  g  h");
+    }
+
+    fn reset_board(&mut self) {
+        use crate::pieces::Pieces::*;
+        for square in &mut self.board {
+            *square = e;
+        }
     }
 
     fn get_file(square: usize) -> usize {
         square & 7
     }
-    
+
     fn get_rank(square: usize) -> usize {
         square >> 4
     }
-    
+
     fn get_square(rank: usize, file: usize) -> usize {
         (rank << 4) + file
+    }
+
+    fn set_square(&mut self, rank: usize, file: usize, piece: Pieces) {
+        let square = Self::get_square(rank, file);
+        self.board[square] = piece;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod pieces;
 use board::Board;
 
 fn main() {
-    let board = Board::new();
+    let mut board = Board::new();
+    board.set_board_from_fen("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - ");
     board.print_board();
 }
