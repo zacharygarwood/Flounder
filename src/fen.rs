57e8a0a247f3a847e0eab8f4f67850e968c10446
Zachary Garwood <garwoodzachary@gmail.com>
2023-05-19 16:17:53 -0500

    Added Result type instead of panic for error checking

--- src/fen.rs
+++ src/fen.rs
@@ -1,5 +1,6 @@
 use crate::board::{Board, Position, Castle};
 use crate::pieces::{Piece, Color};
+use core::result::Result;
 
 // pub fn fen_to_board(fen: &str) -> Board {
 //     let fen_parts: Vec<&str> = fen.split(' ').collect();
@@ -13,9 +14,13 @@ use crate::pieces::{Piece, Color};
 // }
 
 // Format <rank8>'/'<rank7>'/'<rank6>'/'<rank5>'/'<rank4>'/'<rank3>'/'<rank2>'/'<rank1>
-fn parse_piece_placement(piece_placement: &str) -> Position {
+fn parse_piece_placement(piece_placement: &str) -> Result<Position, String> {
     let mut position = Position::new();
     let pieces_placement_rank: Vec<&str> = piece_placement.split('/').collect();
+
+    if pieces_placement_rank.len() != 8 {
+        return Err("Invalid number of ranks in FEN piece placement".to_string())
+    }
 
     for (idx, values) in pieces_placement_rank.iter().enumerate() {
         let rank = 7 - idx as u8;
@@ -28,31 +33,35 @@ fn parse_piece_placement(piece_placement: &str) -> Position {
                     file += 1;
                 } 
                 '1'..='8' => file += c.to_digit(10).unwrap() as u8,
-                _ => panic!("Invalid character in fen piece placement")
+                _ => return Err("Invalid character in FEN piece placement".to_string())
             };
         }
     }
-    position
+    Ok(position)
 }
 
 // Format 'w' | 'b'
-fn parse_active_color(active_color: &str) -> Color {
+fn parse_active_color(active_color: &str) -> Result<Color, String> {
     let c = active_color.chars().next().unwrap();
     match c {
-        'w' => Color::White,
-        'b' => Color::Black,
-        _ => panic!("Invalid character in fen active color")
+        'w' => Ok(Color::White),
+        'b' => Ok(Color::Black),
+        _ => Err("Invalid character in FEN active color".to_string())
     }
 }
 
 // Format '-' | ['K'] ['Q'] ['k'] ['q']
-pub fn parse_castling_ability(castling_ability: &str) -> Castle {
+pub fn parse_castling_ability(castling_ability: &str) -> Result<Castle, String> {
+    if castling_ability.chars().count() > 4 {
+        return Err("Invalid number castling characters in FEN".to_string())
+    }
+
     // Rights will be off in the event of '-' and set on accordingly
     let mut castle_rights = Castle::new(false, false, false, false);
     for c in castling_ability.chars() {
         castle_rights.set(c, true);
     }
-    castle_rights
+    Ok(castle_rights)
 }
 
 // fn parse_en_passant_target(en_passant_target: &str) -> Option<Square> {
