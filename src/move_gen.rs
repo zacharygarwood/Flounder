228c536aa12b7b80f4d8a186b7eed077d34db572
Zachary Garwood <garwoodzachary@gmail.com>
2023-06-18 12:38:24 -0500

    Added perft unit tests

--- src/move_gen.rs
+++ src/move_gen.rs
@@ -483,3 +483,90 @@ impl PawnDirection {
         Self { rank_7, rank_3, north }
     }
 }
+
+
+#[cfg(test)]
+mod tests {
+    use crate::board::Board;
+    use crate::move_gen::MoveGenerator;
+
+    // Positions and results can be found here
+    // https://www.chessprogramming.org/Perft_Results
+    #[test]
+    fn perft_position_1() {
+        let board = Board::new("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+        let move_gen = MoveGenerator::new();
+
+        assert_eq!(move_gen.run_perft(&board, 0), 1);
+        assert_eq!(move_gen.run_perft(&board, 1), 20);
+        assert_eq!(move_gen.run_perft(&board, 2), 400);
+        assert_eq!(move_gen.run_perft(&board, 3), 8902);
+        assert_eq!(move_gen.run_perft(&board, 4), 197281);
+        assert_eq!(move_gen.run_perft(&board, 5), 4865609);
+        assert_eq!(move_gen.run_perft(&board, 6), 119060324);
+    }
+
+    #[test]
+    fn perft_position_2() {
+        let board = Board::new("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1");
+        let move_gen = MoveGenerator::new();
+
+        assert_eq!(move_gen.run_perft(&board, 1), 48);
+        assert_eq!(move_gen.run_perft(&board, 2), 2039);
+        assert_eq!(move_gen.run_perft(&board, 3), 97862);
+        assert_eq!(move_gen.run_perft(&board, 4), 4085603);
+        assert_eq!(move_gen.run_perft(&board, 5), 193690690);
+    }
+
+    #[test]
+    fn perft_position_3() {
+        let board = Board::new("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1");
+        let move_gen = MoveGenerator::new();
+
+        assert_eq!(move_gen.run_perft(&board, 1), 14);
+        assert_eq!(move_gen.run_perft(&board, 2), 191);
+        assert_eq!(move_gen.run_perft(&board, 3), 2812);
+        assert_eq!(move_gen.run_perft(&board, 4), 43238);
+        assert_eq!(move_gen.run_perft(&board, 5), 674624);
+        assert_eq!(move_gen.run_perft(&board, 6), 11030083);
+        assert_eq!(move_gen.run_perft(&board, 7), 178633661);
+    }
+
+    #[test]
+    fn perft_position_4() {
+        let board = Board::new("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1");
+        let move_gen = MoveGenerator::new();
+
+        assert_eq!(move_gen.run_perft(&board, 1), 6);
+        assert_eq!(move_gen.run_perft(&board, 2), 264);
+        assert_eq!(move_gen.run_perft(&board, 3), 9467);
+        assert_eq!(move_gen.run_perft(&board, 4), 422333);
+        assert_eq!(move_gen.run_perft(&board, 5), 15833292);
+        assert_eq!(move_gen.run_perft(&board, 6), 706045033);
+    }
+
+    #[test]
+    fn perft_position_5() {
+        let board = Board::new("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8");
+        let move_gen = MoveGenerator::new();
+
+        assert_eq!(move_gen.run_perft(&board, 1), 44);
+        assert_eq!(move_gen.run_perft(&board, 2), 1486);
+        assert_eq!(move_gen.run_perft(&board, 3), 62379);
+        assert_eq!(move_gen.run_perft(&board, 4), 2103487);
+        assert_eq!(move_gen.run_perft(&board, 5), 89941194);
+    }
+
+    #[test]
+    fn perft_position_6() {
+        let board = Board::new("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10");
+        let move_gen = MoveGenerator::new();
+
+        assert_eq!(move_gen.run_perft(&board, 0), 1);
+        assert_eq!(move_gen.run_perft(&board, 1), 46);
+        assert_eq!(move_gen.run_perft(&board, 2), 2079);
+        assert_eq!(move_gen.run_perft(&board, 3), 89890);
+        assert_eq!(move_gen.run_perft(&board, 4), 3894594);
+        assert_eq!(move_gen.run_perft(&board, 5), 164075551);
+    }
+}
