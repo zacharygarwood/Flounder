fe64507ecd69e88ad394d99187e886e7455efd09
Zachary Garwood <garwoodzachary@gmail.com>
2023-06-19 18:19:21 -0500

    Added unit tests for searching the best move

--- src/search.rs
+++ src/search.rs
@@ -204,3 +204,180 @@ pub fn mvv_lva_sort_moves(board: &Board, moves: &mut [Move]) {
         0
     })
 }
+
+
+#[cfg(test)]
+mod tests {
+    use crate::board::Board;
+    use crate::search::Searcher;
+
+    const DEPTH: u8 = 6;
+
+
+    // Positions found here:
+    // https://lichess.org/practice/checkmates/checkmate-patterns-iii/
+    #[test]
+    fn opera_mate_1() {
+        let board = Board::new("4k3/5p2/8/6B1/8/8/8/3R2K1 w - - 0 1");
+        let mut searcher = Searcher::new();
+        let best_move = searcher.best_move(&board, DEPTH).1.unwrap();
+
+        assert_eq!(best_move.to_algebraic(), "d1d8");
+    }
+
+    #[test]
+    fn opera_mate_2() {
+        let board = Board::new("rn1r2k1/ppp2ppp/3q1n2/4b1B1/4P1b1/1BP1Q3/PP3PPP/RN2K1NR b KQ - 0 1");
+        let mut searcher = Searcher::new();
+        let best_move = searcher.best_move(&board, DEPTH).1.unwrap();
+
+        assert_eq!(best_move.to_algebraic(), "d6d1");
+    }
+
+    #[test]
+    fn opera_mate_3() {
+        let board = Board::new("rn3rk1/p5pp/2p5/3Ppb2/2q5/1Q6/PPPB2PP/R3K1NR b KQ - 0 1");
+        let mut searcher = Searcher::new();
+        let best_move = searcher.best_move(&board, DEPTH).1.unwrap();
+
+        assert_eq!(best_move.to_algebraic(), "c4f1");
+    }
+
+    #[test]
+    fn anderssens_mate_1() {
+        let board = Board::new("6k1/6P1/5K1R/8/8/8/8/8 w - - 0 1");
+        let mut searcher = Searcher::new();
+        let best_move = searcher.best_move(&board, DEPTH).1.unwrap();
+
+        assert_eq!(best_move.to_algebraic(), "h6h8");
+    }
+
+    #[test]
+    fn anderssens_mate_2() {
+        let board = Board::new("1k2r3/pP3pp1/8/3P1B1p/5q2/N1P2b2/PP3Pp1/R5K1 b - - 0 1");
+        let mut searcher = Searcher::new();
+        let best_move = searcher.best_move(&board, DEPTH).1.unwrap();
+
+        assert_eq!(best_move.to_algebraic(), "f4h4");
+    }
+
+    #[test]
+    fn anderssens_mate_3() {
+        let board = Board::new("2r1nrk1/p4p1p/1p2p1pQ/nPqbRN2/8/P2B4/1BP2PPP/3R2K1 w - - 0 1");
+        let mut searcher = Searcher::new();
+        let best_move = searcher.best_move(&board, DEPTH).1.unwrap();
+
+        assert_eq!(best_move.to_algebraic(), "f5e7");
+    }
+
+    #[test]
+    fn dovetail_mate_1() {
+        let board = Board::new("1r6/pk6/4Q3/3P4/8/8/8/6K1 w - - 0 1");
+        let mut searcher = Searcher::new();
+        let best_move = searcher.best_move(&board, DEPTH).1.unwrap();
+
+        assert_eq!(best_move.to_algebraic(), "e6c6");
+    }
+
+    #[test]
+    fn dovetail_mate_2() {
+        let board = Board::new("r1b1q1r1/ppp3kp/1bnp4/4p1B1/3PP3/2P2Q2/PP3PPP/RN3RK1 w - - 0 1");
+        let mut searcher = Searcher::new();
+        let best_move = searcher.best_move(&board, DEPTH).1.unwrap();
+
+        assert_eq!(best_move.to_algebraic(), "f3f6");
+    }
+
+    #[test]
+    fn dovetail_mate_3() {
+        let board = Board::new("6k1/1p1b3p/2pp2p1/p7/2Pb2Pq/1P1PpK2/P1N3RP/1RQ5 b - - 0 1");
+        let mut searcher = Searcher::new();
+        let best_move = searcher.best_move(&board, DEPTH).1.unwrap();
+
+        assert_eq!(best_move.to_algebraic(), "d7g4");
+    }
+
+    #[test]
+    fn dovetail_mate_4() {
+        let board = Board::new("rR6/5k2/2p3q1/4Qpb1/2PB1Pb1/4P3/r5R1/6K1 w - - 0 1");
+        let mut searcher = Searcher::new();
+        let best_move = searcher.best_move(&board, DEPTH).1.unwrap();
+
+        assert_eq!(best_move.to_algebraic(), "e5e8");
+    }
+
+    #[test]
+    fn cozios_mate_1() {
+        let board = Board::new("8/8/1Q6/8/6pk/5q2/8/6K1 w - - 0 1");
+        let mut searcher = Searcher::new();
+        let best_move = searcher.best_move(&board, DEPTH).1.unwrap();
+
+        assert_eq!(best_move.to_algebraic(), "b6h6");
+    }
+
+    #[test]
+    fn swallows_tail_mate_1() {
+        let board = Board::new("3r1r2/4k3/R7/3Q4/8/8/8/6K1 w - - 0 1");
+        let mut searcher = Searcher::new();
+        let best_move = searcher.best_move(&board, DEPTH).1.unwrap();
+
+        assert_eq!(best_move.to_algebraic(), "d5e6");
+    }
+
+    #[test]
+    fn swallows_tail_mate_2() {
+        let board = Board::new("8/8/2P5/3K1k2/2R3p1/2q5/8/8 b - - 0 1");
+        let mut searcher = Searcher::new();
+        let best_move = searcher.best_move(&board, DEPTH).1.unwrap();
+
+        assert_eq!(best_move.to_algebraic(), "c3e5");
+    }
+
+    #[test]
+    fn epaulette_mate_1() {
+        let board = Board::new("3rkr2/8/5Q2/8/8/8/8/6K1 w - - 0 1");
+        let mut searcher = Searcher::new();
+        let best_move = searcher.best_move(&board, DEPTH).1.unwrap();
+
+        assert_eq!(best_move.to_algebraic(), "f6e6");
+    }
+
+    #[test]
+    fn epaulette_mate_2() {
+        let board = Board::new("1k1r4/pp1q1B1p/3bQp2/2p2r2/P6P/2BnP3/1P6/5RKR b - - 0 1");
+        let mut searcher = Searcher::new();
+        let best_move = searcher.best_move(&board, DEPTH).1.unwrap();
+
+        assert_eq!(best_move.to_algebraic(), "d8g8");
+    }
+
+    #[test]
+    fn epaulette_mate_3() {
+        let board = Board::new("5r2/pp3k2/5r2/q1p2Q2/3P4/6R1/PPP2PP1/1K6 w - - 0 1");
+        let mut searcher = Searcher::new();
+        let best_move = searcher.best_move(&board, DEPTH).1.unwrap();
+
+        assert_eq!(best_move.to_algebraic(), "f5d7");
+    }
+
+    #[test]
+    fn pawn_mate_1() {
+        let board = Board::new("8/7R/1pkp4/2p5/1PP5/8/8/6K1 w - - 0 1");
+        let mut searcher = Searcher::new();
+        let best_move = searcher.best_move(&board, DEPTH).1.unwrap();
+
+        assert_eq!(best_move.to_algebraic(), "b4b5");
+    }
+
+    #[test]
+    fn pawn_mate_2() {
+        let board = Board::new("r1b3nr/ppp3qp/1bnpk3/4p1BQ/3PP3/2P5/PP3PPP/RN3RK1 w - - 0 11");
+        let mut searcher = Searcher::new();
+        let best_move = searcher.best_move(&board, DEPTH).1.unwrap();
+
+        assert_eq!(best_move.to_algebraic(), "h5e8");
+    }
+    
+
+    
+}
